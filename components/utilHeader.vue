79b622f8ed8b1bc942f0244dfc6238801e6a200b
Masaki <ryota423.jp@icloud.com>
2024-09-10 22:03:52 +0900

    add:tailwindcssセットアップとヘッダーの仮組み

---
+++ components/utilHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex justify-between">
+    <!-- サイトタイトル -->
+    <div>
+      <h1>タイトル</h1>
+    </div>
+    <!-- サイトのボタン群 -->
+    <div class="flex">
+      <p>ボタン1</p>
+      <p>ボタン2</p>
+      <p>ボタン3</p>
+      <p>ボタン4</p>
+    </div>
+  </div>
+</template>
---
+++ pages/sample/testPage.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>テストページ</h1>
+  <NuxtLink :to="{ name: 'index' }">ホームに戻る</NuxtLink>
+</template>
